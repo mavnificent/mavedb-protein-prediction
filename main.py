4dd425adfbceeae41588ab4a015b76eb803de629
Tyler Thompson <tylernt1199@gmail.com>
2025-09-21 00:57:11 -0500

    add dataset stuff to main

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from data import sequence_builder
+from data.dataset import ProteinDataset
 from pathlib import Path
 
 if not Path('data/Sequences.csv').exists():
     sequence_builder.build_sequence_csv()
+
+one_hot_dataset = ProteinDataset('train', encoding='one-hot')
 
